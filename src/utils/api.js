80bda964ad2702addaf220dc7d6b1c237d00c377
Kai Duan <kai.duan.com@gmail.com>
2018-11-19 23:03:59 +0800

    get status

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,11 +11,22 @@ export default{
         url: 'https://www.easy-mock.com/mock/5bed59d57939e615b8bbdbc6/mock/login',
         method: 'post',
         complete: (res) => {
-          if (res.data.data.status === 1) {
-            return true
-          } else {
-            return false
-          }
+          resolve(res)
+        }
+      })
+    })
+  },
+  register (reqData) {
+    return new Promise((resolve, reject) => {
+      wepy.request({
+        header: {
+          'content-type': 'application/json'
+        },
+        data: reqData,
+        url: 'https://www.easy-mock.com/mock/5bed59d57939e615b8bbdbc6/mock/register',
+        method: 'post',
+        complete: (res) => {
+          resolve(res)
         }
       })
     })
